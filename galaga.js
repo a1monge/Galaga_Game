8a4cba49cecc637d25ed7bc8f7cc9aacb801fd4f
Alex Monge <147656286+a1monge@users.noreply.github.com>
2024-09-06 14:59:59 -0500

    Update galaga.js

--- galaga.js
+++ galaga.js
@@ -453,20 +453,20 @@ function animate() {
 
 animate()
 
-addEventListener('keydown', ({ key }) => {
+addEventListener('keydown', (event) => {
     if (game.over) return
-    switch (key) {
+    switch (event.key) {
         case 'a':
             console.log('left');
-
             keys.a.pressed = true;
             break;
         case 'd':
-            console.log('right')
+            console.log('right');
             keys.d.pressed = true;
             break;
         case ' ':
-            console.log('space')
+            event.preventDefault(); // Prevents the default behavior of the spacebar (scrolling)
+            console.log('space');
             projectiles.push(new Projectile({
                 position: {
                     x: player.position.x + player.width / 2,
@@ -476,10 +476,10 @@ addEventListener('keydown', ({ key }) => {
                     x: 0,
                     y: -10
                 }
-            }))
+            }));
             break;
     }
-})
+});
 
 addEventListener('keyup', ({ key }) => {
     switch (key) {
